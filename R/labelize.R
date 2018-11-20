f89c961664a6da9b51c635e9d839e96a459e4898
mlgrm <3696450+mlgrm@users.noreply.github.com>
2018-11-20 15:00:36 +0100

    speeded up labelize

--- R/labelize.R
+++ R/labelize.R
@@ -7,25 +7,23 @@ labelize <- function(x) UseMethod("labelize",x)
 #' 
 #' @export
 labelize.svy <-  
-  function(s){
-    s <- preserve(s,function(x)lapply(x,labelize))
+  function(s, lang = getOption("svyLang", "English")){
+    s <- preserve(s,function(x)tibble::as_tibble(llply(x,labelize), 
+                                                 validate = F))
     s
   }
 
 #' s3 method for class svq
 #' 
 #' @export
-labelize.svq <- function(q){
+labelize.svq <- function(q, lang = getOption("svyLang", "English")){
   switch(type(q),
          "select one"={
-           # message(paste(group(q),collapse = "."),".",name(q))
-           # if(name(q)=="cooks_gender") browser()
-           q <- preserve(q,function(x)
-             factor(x,levels=levels(x),labels=labels(x)))
+           levels(q) <- labels(q, lang = lang)
            q
          },
          "select all that apply"={
-           colnames(q) <- labels(q)
+           colnames(q) <- labels(q, lang = lang)
            q
          },
          q
