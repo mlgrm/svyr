9486b5afad1a6e2f31fdba2eed145bbb79f40779
mlgrm <3696450+mlgrm@users.noreply.github.com>
2018-12-17 10:30:18 +0100

    don't override bind_rows

--- R/bind.R
+++ R/bind.R
@@ -1,14 +1,14 @@
 #' row bind two or more `svy`s together
 #' 
 #' @importFrom dplyr bind_cols
-#' @export
-bind_rows <- function(x, ...)UseMethod("bind_rows")
-
-bind_rows.svy <- function(...){
-  args <- list(...)
-  suppressWarnings(dplyr::bind_rows(...)) %>% 
-    llply(function(q)
-  
-}
-
-bind_rows.default <- dplyr::bind_rows
+#' #' @export
+#' bind_rows <- function(x, ...)UseMethod("bind_rows")
+#' 
+#' bind_rows.svy <- function(...){
+#'   args <- list(...)
+#'   suppressWarnings(dplyr::bind_rows(...)) %>% 
+#'     llply(function(q)
+#'   
+#' }
+#' 
+#' bind_rows.default <- dplyr::bind_rows
